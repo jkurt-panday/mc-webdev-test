2e004569cfd8c1f65aa01f92f446c78bc5541d36
kurt_ <pandaykurt427@gmail.com>
2025-07-05 19:22:30 +0800

    Add navigate function

--- bank/app.js
+++ bank/app.js
@@ -1,10 +1,31 @@
 // displaying template elements
 
-function updateRoute(templateId) {
-    const template = document.getElementById(templateId);
+const routes = {
+    '/login': {templateId: 'login'},
+    '/dashboard': {templateId: 'dashboard'}
+};
+
+function updateRoute() {
+    const path = window.location.pathname;
+    const route = routes[path];
+    
+    // if a route cannot be found, redirect to 'login' page
+    if (!route) {
+        return navigate('/login')
+    }
+  
+    const template = document.getElementById(route.templateId);
     const view = template.content.cloneNode(true);
     const app = document.getElementById('app');
     
     app.innerHTML = '';
     app.appendChild(view);
+}
+
+updateRoute()
+
+
+function navigate(path) {
+    window.history.pushState({}, path, path);
+    updateRoute();  
 }
