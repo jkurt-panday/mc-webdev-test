ebe28b806e7b25c7c8c71e08465d4fddf4697033
kurt_ <pandaykurt427@gmail.com>
2025-07-05 17:49:45 +0800

    Bank app javascript file

---
+++ bank/app.js
@@ -0,0 +1,10 @@
+// displaying template elements
+
+function updateRoute(templateId) {
+    const template = document.getElementById(templateId);
+    const view = template.content.cloneNode(true);
+    const app = document.getElementById('app');
+    
+    app.innerHTML = '';
+    app.appendChild(view);
+}
