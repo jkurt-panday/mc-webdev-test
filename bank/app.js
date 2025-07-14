5948c9dc0a71853d34a226fb2eca89ae4d7e0cd1
kurt_ <pandaykurt427@gmail.com>
2025-07-15 07:56:09 +0800

    Add register function for form

--- bank/app.js
+++ bank/app.js
@@ -41,3 +41,16 @@ function onLinkClick(event) {
 
 window.onpopstate = () => updateRoute()
 updateRoute()
+
+
+// register function that sends data to the server
+function register() {
+    const registerForm = document.getElementById('registerForm');
+    // FormData interface is built in
+    const formData = new FormData(registerForm);
+    console.log(formData)
+    const data = Object.fromEntries(formData);
+    console.log(data)
+    const jsonData = JSON.stringify(data)
+    console.log(jsonData);
+}
